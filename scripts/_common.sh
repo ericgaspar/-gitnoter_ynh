84b4d96da80f741bf7660ceb4a01b5e9bc910adc
ericgaspar <junk.eg@free.fr>
2022-06-01 17:51:04 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
+# nodejs version
+nodejs_version=18
+
 # dependencies used by the app
 pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
